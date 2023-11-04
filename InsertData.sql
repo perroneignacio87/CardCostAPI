a1e4f0a200914b900519db68dd117b96061e4444
ignacioperrone <ignacio.perrone@accordlms.com>
2023-11-04 16:06:35 -0300

    insert data script

---
+++ InsertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO CountryCost (Country, Cost) values ('US', 5.0)
+INSERT INTO CountryCost (Country, Cost) values ('GR', 15.0)
+INSERT INTO CountryCost (Country, Cost) values ('Others', 10.0)
